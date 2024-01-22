639bcba4d86ace7ae1ca202aa86b0bff281adcd3
Joao Freitas <joaovitor6586@gmail.com>
2024-01-22 01:46:48 -0300

    added Viewsets and Tests

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -27,6 +27,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+}
 
 # Application definition
 
@@ -37,8 +46,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    'django_extensions',
     "order",
     "product",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
---
+++ order/viewsets/order_viewset.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from rest_framework import response
+
+from order.models import Order
+from order.serializers import OrderSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all().order_by("id")
