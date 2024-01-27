3f13ab27cb4401124bb7bed37d8e49b7deedd7c0
Joao Freitas <joaovitor6586@gmail.com>
2024-01-27 01:57:12 -0300

    added allowed Hosts

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -153,7 +153,7 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = ["localhost","127.0.0.1","ebac-bookstore.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","ebac-bookstore.herokuapp.com","john.pythonanywhere.com"]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
